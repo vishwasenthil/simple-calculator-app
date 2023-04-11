bb13c88a8c2469371f1d5f10e683f6846f76c8a1
vishwasenthil <122578261+vishwasenthil@users.noreply.github.com>
2023-04-11 05:25:13 +0000

    added functionality for each operator button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import './App.css';
 
 function App() {
@@ -8,10 +8,42 @@ function App() {
   let operators = ["+", "-", "*", "/", "c", "ac"];
   let [currOperator, setCurrOperator] = useState(``);
 
+  let memory = useRef([]);
+
   function handleClick(e) {
     //TODO: finish function
     e.preventDefault();
-    console.log(currOperator);
+    switch(currOperator) {
+      case `+`:
+        setAnswer(answer+Number(input)); //Number(input) b/c input gets changed to string when value is entered
+        break;
+      case `-`:
+        setAnswer(answer-Number(input)); //Number(input) b/c input gets changed to string when value is entered
+        break;
+      case `*`:
+        setAnswer(answer*Number(input)); //Number(input) b/c input gets changed to string when value is entered
+        break;
+      case `/`:
+        setAnswer(answer/Number(input)); //Number(input) b/c input gets changed to string when value is entered
+        break;
+      case `c`:
+        setAnswer(0);
+        break;
+      case `ac`:
+        memory.current.length = 0;
+        setAnswer(0);
+        break;
+    }
+
+    setInput(0);
+    //updating memory
+    if(currOperator != `ac`) {
+      memory.current = [...memory.current, {
+        equation:`${answer+currOperator+input}`,
+        answer:answer
+      }];
+    }
+    console.log(memory.current);
   }
 
   return (
@@ -21,12 +53,12 @@ function App() {
       </header>
       <main>
         <form onSubmit={handleClick}>
-          <input type="number" onChange={e=>setInput(e.target.value)}/>
+          <input type="number" value={input} onChange={e=>setInput(e.target.value)}/>
           <p>{answer}</p>
           {
             operators.map(operator=>{
               return (
-                <button key={operator} type="submit" onClick={()=>setCurrOperator({operator})}>{operator}</button>
+                <button key={operator} type="submit" onClick={()=>setCurrOperator(operator)}>{operator}</button>
               );
             })
           }
